750be537ef7ecf5166ad984de39ac0fd5c77fba7
chen-liang <cl1337339572@gmail.com>
2019-12-23 11:02:37 +0800

    二分法

--- 剑指offer/旋转数组的最小数字.cpp
+++ 剑指offer/旋转数组的最小数字.cpp
@@ -9,3 +9,26 @@ public:
         return rotateArray[0];
     }
 };
+class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+        if(rotateArray.size()==0) return 0;
+        int left=0,right=rotateArray.size()-1,mid;
+        while(left<right)
+        {
+            mid=left+(right-left)/2;
+            if(rotateArray[right]<rotateArray[mid])
+            {
+                left=mid+1;
+            }
+            else if(rotateArray[right]==rotateArray[mid])
+            {
+                right=right-1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return rotateArray[left];
+    }
+};
