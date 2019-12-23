9f24c3336281e2309e9fd9959a94651167342849
chen-liang <cl1337339572@gmail.com>
2019-12-23 16:21:58 +0800

    前序

--- 剑指offer/二叉树6种遍历方法.cpp
+++ 剑指offer/二叉树6种遍历方法.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<malloc.h>
 using namespace std;
 struct TreeNode{
     char ch;
@@ -6,7 +7,8 @@ struct TreeNode{
     TreeNode *right;
     TreeNode(char x):ch(x),left(NULL),right(NULL){}
 };
-TreeNode *create(TreeNode *T)
+//树的创建
+void create(TreeNode * &T)
 {
     char ch;
     cin>>ch;
@@ -17,14 +19,20 @@ TreeNode *create(TreeNode *T)
         T->ch=ch;
         create(T->left);
         create(T->right);
-        return T;
     }
-    
-    return NULL;
+}
+//递归前序、中序、后序遍历
+void PreOrder(TreeNode *T)
+{
+    if(T==NULL) return;
+    cout<<T->ch<<" ";
+    PreOrder(T->left);
+    PreOrder(T->right);
 }
 int main(void)
 {
     TreeNode *Tree;
     create(Tree);
+    PreOrder(Tree);
     return 0;
 }
