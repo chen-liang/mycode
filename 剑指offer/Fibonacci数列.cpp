421e0ad1da72d085e03f1e398d4b05993aa1e073
chen-liang <cl1337339572@gmail.com>
2019-12-25 20:31:16 +0800

    动态规划实现Fibonacci

--- 剑指offer/Fibonacci数列.cpp
+++ 剑指offer/Fibonacci数列.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-class Solution {
+/*class Solution {
 public:
     int Fibonacci(int n) {
         if(n<1)
@@ -8,11 +9,11 @@ public:
              return 0;
         }
         else if(n==1||n==2) return 1;
-       /* else if(n>=3&&n<=39)
-        {
-           return Fibonacci(n-1)+Fibonacci(n-2);
-        }
-        */
+        //else if(n>=3&&n<=39)
+        // {
+        // return Fibonacci(n-1)+Fibonacci(n-2);
+       // }
+        
        int res=1,pre=1,temp=0;
        for(int i=3;i<=n;i++)
        {
@@ -24,6 +25,23 @@ public:
        
     }
 };
+*/
+class Solution
+{
+    public:
+        int Fibonacci(int n){
+            vector<int> f(40,0);
+            f[1]=1;f[2]=1;
+            for(int i=3;i<40;i++)
+            {
+                f[i]=f[i-1]+f[i-2];
+            }
+            return f[n];
+        }
+
+};
+
+
 int main()
 {
     Solution S;
