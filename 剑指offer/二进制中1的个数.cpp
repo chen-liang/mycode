29e4e72d985fbbe7904fc874fa5b22e7abe59a18
chen-liang <cl1337339572@gmail.com>
2019-12-28 20:56:28 +0800

    二进制中1的个数

---
+++ 剑指offer/二进制中1的个数.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+     int  NumberOf1(int n)
+     {
+        int count=0;
+        while(n!=0)
+        {
+            ++count;
+            n=n&(n-1);
+        }
+        return count;
+     }
+};
+
+
