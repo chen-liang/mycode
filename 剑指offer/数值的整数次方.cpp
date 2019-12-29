d99c422148a7ae2b136968e27b3d9b376fefb2b3
chen-liang <cl1337339572@gmail.com>
2019-12-29 19:50:17 +0800

    11.

--- 剑指offer/数值的整数次方.cpp
+++ 剑指offer/数值的整数次方.cpp
@@ -6,3 +6,25 @@ public:
         return pow(base,exponent);
     }
 };
+/*
+public double Power(double base, int n) {
+    double res = 1,curr = base;
+    int exponent;
+    if(n>0){
+        exponent = n;
+    }else if(n<0){
+        if(base==0)
+            throw new RuntimeException("分母不能为0"); 
+        exponent = -n;
+    }else{// n==0
+        return 1;// 0的0次方
+    }
+    while(exponent!=0){
+        if((exponent&1)==1)
+            res*=curr;
+        curr*=curr;// 翻倍
+        exponent>>=1;// 右移一位
+    }
+    return n>=0?res:(1/res);       
+}
+*/
