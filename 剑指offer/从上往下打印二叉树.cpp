620e5391c76c07f7688e59e474777944ca72ba32
chen-liang <cl1337339572@gmail.com>
2020-01-11 09:21:42 +0800

    从上往下打印二叉树

---
+++ 剑指offer/从上往下打印二叉树.cpp
@@ -0,0 +1,20 @@
+/*
+从上往下打印出二叉树的每个节点，同层节点从左至右打印
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+    vector<int> PrintFromTopToBottom(TreeNode* root) {
+
+    }
+};
